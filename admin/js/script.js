66589107c331d6638bb40d3a3032a7dc5297f68c
JohnLloyd2335 <johnlloydlacadin42301@gmail.com>
2024-05-07 12:31:18 +0800

    pending rental done

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
 
     //Toggle Icon
@@ -48,6 +50,123 @@ $(document).ready(function () {
         sessionMessageContainer.toggleClass('show-session-message');
     });
 
+    //Carousel Container
+    $('#carousel-button-next').click(function () {
+        let curImg = $(".active-carousel-car-image");
+        let nextImg = curImg.next();
+
+        if (nextImg.length) {
+            $(this).css('cursor', 'pointer');
+            curImg.removeClass("active-carousel-car-image");
+            nextImg.addClass("active-carousel-car-image");
+        }
+    });
+
+    $('#carousel-button-prev').click(function () {
+        let curImg = $(".active-carousel-car-image");
+        let prevImg = curImg.prev();
+
+        if (prevImg.length) {
+            $(this).css('cursor', 'pointer');
+            curImg.removeClass("active-carousel-car-image");
+            prevImg.addClass("active-carousel-car-image");
+        }
+    });
+
+    //view car tabs
+
+    //car tabs
+    $("#car-accessories-header").click(function () {
+        $(this).addClass("tab-item-header-active");
+        $("#car-description-header").removeClass("tab-item-header-active");
+        $("#car-description-content").hide();
+        $("#car-accessories-content").show();
+    });
+
+    $("#car-description-header").click(function () {
+        $(this).addClass("tab-item-header-active");
+        $("#car-accessories-header").removeClass("tab-item-header-active");
+        $("#car-accessories-content").hide();
+        $("#car-description-content").show();
+    });
+
 
 
 });
+
+function setCarUnavail(id) {
+
+    Swal.fire({
+        title: "Warning!",
+        text: "Are you sure you want to set Unavailable?",
+        icon: "info",
+        showCancelButton: true,
+        confirmButtonText: "Yes"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            console.log("confirmed");
+        }
+        else {
+            console.log("cancelled");
+        }
+    });
+
+}
+
+function approveCarRental(id) {
+
+    Swal.fire({
+        title: "Warning!",
+        text: "Are you sure you want to Approve?",
+        icon: "info",
+        showCancelButton: true,
+        confirmButtonText: "Yes"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            console.log("confirmed");
+        }
+        else {
+            console.log("cancelled");
+        }
+    });
+
+}
+
+function cancelCarRental(id) {
+    Swal.fire({
+        title: "Are you sure you want to cancel?",
+        text: "Are you sure you want to Cancel?",
+        icon: "info",
+        showCancelButton: true,
+        confirmButtonText: "Yes",
+        html: "<textarea id='cancel-reason' placeholder='Reason' style='width: 100%; height: 100px;resize:none;padding-left: 10px; padding-top:5px;font-size:18px; margin-top: 30px'></textarea>"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let cancelReason = $("#cancel-reason").val();
+            console.log(cancelReason);
+            console.log("confirmed");
+        }
+        else {
+            console.log("cancelled");
+        }
+    });
+}
+
+function markAsCompletedRental(id) {
+    Swal.fire({
+        title: "Warning",
+        text: "Are you sure you want to mark as completed?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes"
+    }).then((result) => {
+
+        if (result.isConfirmed) {
+            console.log("completed")
+        }
+        else {
+            console.log("cancelled")
+        }
+
+    });
+}
